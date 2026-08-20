42401e1a2543d54024ca2db7ac057994f252f7b1
Cassandra Kao <cassandra093517@gmail.com>
2026-08-20 18:58:12 -0400

    update api to connect with sqlite db

--- ai_api.py
+++ ai_api.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 import pandas as pd
+import sqlite3
 
 app=FastAPI()
 
 @app.get('/')
 
-def welcome():
-  df=pd.read_csv('cleaned_ai_data.csv')
+def get_ai_tools():
+  connect = sqlite3.connect('ai_Dashboard.db')
+
+  sql_query='SELECT * FROM ai_tools'
+
+  df=pd.read_sql_query(sql_query,connect)
+
+  connect.close()
+
+
   data_dict=df.head(5).to_dict(orient='records')
   return data_dict
+
