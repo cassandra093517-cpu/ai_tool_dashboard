bdbe85ff23cf2d2c4ee9829ad18f51ed2231bf24
Cassandra Kao <cassandra093517@gmail.com>
2026-08-20 19:42:37 -0400

    update 2nd api to filter out pricing models

--- ai_api.py
+++ ai_api.py
@@ -19,3 +19,12 @@ def get_ai_tools():
   data_dict=df.head(5).to_dict(orient='records')
   return data_dict
 
+@app.get('/pricing/{price_type}')
+
+def get_tools_by_priceModel(price_type: str):
+  connect = sqlite3.connect('ai_Dashboard.db')
+  sql_query="SELECT * FROM ai_tools WHERE Pricing_Model=?"
+  df=pd.read_sql_query(sql_query,connect, params=(price_type,))
+
+  connect.close()
+  return df.to_dict(orient='records')
