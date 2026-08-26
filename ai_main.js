3e086339f367a8ca02c4af105867833080ef95b7
Cassandra Kao <cassandra093517@gmail.com>
2026-08-26 00:16:57 -0400

    added fetch api and added tables

--- ai_main.js
+++ ai_main.js
@@ -1,19 +1,50 @@
-const overview = document.getElementById("nav-overview");
-const traffic = document.getElementById("nav-traffic");
-const pricingChart = document.getElementById("nav-pricing-chart");
-const dataCon = document.getElementById("data_container");
+const search = document.getElementById("apply_filter_btn");
+const price = document.getElementById("pricing_select")
 
-async function fetchTopTraffic() {
+
+
+
+
+async function pricingSelect() {
+
+  const res = price.value;
+  const price_url = `http://127.0.0.1:8000/pricing/${res}`;
+  console.log(price_url);
+
   try {
-    const response = await fetch("http://127.0.0.1:8000/top-traffic");
+    const response = await fetch(price_url);
     const data = await response.json();
     console.log("data received:", data);
-    console.log(data[0])
+    const dataDisplay = document.getElementById("data_container");
+    let dataHtml = `
+    <table border ="1">
+    <thead>
+<tr>
+<th>No.</th>
+<th>Tool Name</th>
+<th>Pricing Model</th>
+</tr>
+    </thead>
+    <tbody>
+    `;
+    for (let i = 0; i < data.length; i++) {
+      dataHtml += `<tr>
+      <th>${i + 1}</th>
+      <th>${data[i].Tool_Name}</th>
+      <th>${data[i].Pricing_Model}</th>
+      </tr>
+      `;
+    }
+
+    dataHtml += `
+</tbody>
+</table>
+`
+    dataDisplay.innerHTML = dataHtml;
   } catch (error) {
     console.error("Fetch failed: ", error);
   }
-
-
 }
 
-overview.addEventListener("click", fetchTopTraffic);
+search.addEventListener("click", pricingSelect);
+
