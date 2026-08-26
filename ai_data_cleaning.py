f5054305891fd0d09f70030b889fa5a99ce36c00
Cassandra Kao <cassandra093517@gmail.com>
2026-08-26 16:32:55 -0400

    update to get top 10 traffic

--- ai_data_cleaning.py
+++ ai_data_cleaning.py
@@ -10,3 +10,4 @@ df["Starting_Price_CAD"] =df["Starting_Price_USD"]*1.37
 df["Starting_Price_CAD"]=df["Starting_Price_CAD"].round(2)
 
 df.to_csv("cleaned_ai_data.csv", index=False)
+# print(df["Pricing_Model"].value_counts())
