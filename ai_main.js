5a35386e8c0bb3457ac3bae73bef3fecc6af1320
Cassandra Kao <cassandra093517@gmail.com>
2026-08-31 22:04:05 -0400

    added early return when empty string added loading state updated data formatting

--- ai_main.js
+++ ai_main.js
@@ -10,25 +10,47 @@ let sortOrder = {
   'User_Rating': true
 };
 
-
+let pricePieChart = null;
 let top10BarChart = null;
+let sort = "";
 
 
 async function fetch_Dashboard() {
   const tool_url = `http://127.0.0.1:8000/api/tools?pricing_model=${price.value}&min_rating=${rating.value}&primary_category=${category.value}`;
   console.log(tool_url);
 
+  const searchbtn = document.getElementById('apply_filter_btn');
 
+  searchbtn.disabled = true;
+  searchbtn.textContent = 'Loading...';
 
 
   try {
     const response = await fetch(tool_url);
     currentData = await response.json();
-    console.log("data received:", currentData);
 
     const toolsCountDispaly = document.getElementById("total_tools");
     const maxTrafficDisplay = document.getElementById("max_traffic");
     const avgRatingDisplay = document.getElementById("avg_rating");
+
+    if (currentData.length === 0) {
+
+      console.log("No data");
+      dataDisplay.innerHTML = `<p>No matching tools found.</p>`;
+      toolsCountDispaly.innerHTML = `<h3>Total Tools</h3><p>0</p>`;
+      maxTrafficDisplay.innerHTML = `<h3>Max Traffic</h3><p>0</p>`;
+      avgRatingDisplay.innerHTML = `<h3>Average Rating</h3><p>0</p>`;
+
+      if (top10BarChart != null) {
+        top10BarChart.destroy();
+      }
+
+      if (pricePieChart != null) {
+        pricePieChart.destroy();
+      }
+      return;
+    }
+    console.log("data received:", currentData);
 
     let total_tools = 0;
     let max_traffic = 0;
@@ -49,13 +71,14 @@ async function fetch_Dashboard() {
     total_tools = i;
     ave_rating = total_rating / total_tools;
 
-
+    let formatMaxTraffic = new Intl.NumberFormat('en-CA').format(max_traffic);
     toolsCountDispaly.innerHTML = `<h3>Total Tools</h3><p>${total_tools}</p>`;
-    maxTrafficDisplay.innerHTML = `<h3>Max Traffic</h3><p>${max_traffic}</p>`;
+    maxTrafficDisplay.innerHTML = `<h3>Max Traffic</h3><p>${formatMaxTraffic}</p>`;
     avgRatingDisplay.innerHTML = `<h3>Average Rating</h3><p>${ave_rating.toFixed(2)}</p>`;
 
 
     render_bar_chart(currentData);
+    render_pie_chart(currentData);
 
 
 
@@ -63,12 +86,31 @@ async function fetch_Dashboard() {
   } catch (error) {
     console.error("Fetch failed: ", error);
   }
+  finally {
+    searchbtn.disabled = false;
+    searchbtn.textContent = 'Search';
+  }
 
 }
 
 search.addEventListener("click", fetch_Dashboard)
 
 async function render_table(data) {
+  let priceArrow = '↕';
+  let trafficArrow = '↕';
+  let ratingArrow = '↕';
+
+  if (sort === 'Starting_Price_CAD') {
+    priceArrow = sortOrder['Starting_Price_CAD'] ? '▲' : '▼';
+  }
+  else if (sort === 'Monthly_Traffic_Est') {
+    trafficArrow = sortOrder['Monthly_Traffic_Est'] ? '▲' : '▼';
+  }
+  else if (sort === 'User_Rating') {
+    ratingArrow = sortOrder['User_Rating'] ? '▲' : '▼';
+  }
+
+
   let dataHtml = `
     <table border ="1">
     <thead>
@@ -77,9 +119,9 @@ async function render_table(data) {
 <th>Tool Name</th>
 <th>Primary_Category</th>
 <th>Pricing Model</th>
-<th onclick="sortTable('Starting_Price_CAD')">Starting Price (CAD)</th>
-<th onclick="sortTable('Monthly_Traffic_Est')">Monthly Estimated Traffic</th>
-<th onclick="sortTable('User_Rating')">User Rating</th>
+<th onclick="sortTable('Starting_Price_CAD')">Starting Price (CAD) ${priceArrow} </th>
+<th onclick="sortTable('Monthly_Traffic_Est')">Monthly Estimated Traffic ${trafficArrow} </th>
+<th onclick="sortTable('User_Rating')">User Rating ${ratingArrow} </th>
 </tr>
     </thead>
     <tbody>
@@ -87,13 +129,22 @@ async function render_table(data) {
   let i = 0
 
   for (i; i < data.length; i++) {
+
+    let commaFormat = new Intl.NumberFormat('en-CA').format(data[i].Monthly_Traffic_Est);
+    let currencyFormat = new Intl.NumberFormat('en-CA',
+      {
+        style: 'currency',
+        currency: 'CAD'
+      }
+    ).format(data[i].Starting_Price_CAD);
+
     dataHtml += `<tr>
       <td>${i + 1}</td>
       <td>${data[i].Tool_Name}</td>
       <td>${data[i].Primary_Category}</td>
       <td>${data[i].Pricing_Model}</td>
-      <td>${data[i].Starting_Price_CAD}</td>
-      <td>${data[i].Monthly_Traffic_Est}</td>
+      <td>${currencyFormat}</td>
+      <td>${commaFormat}</td>
       <td>${data[i].User_Rating}</td>
       </tr>
       `;
@@ -107,8 +158,9 @@ async function render_table(data) {
 }
 
 async function render_bar_chart(data) {
-  data.sort((a, b) => b.Monthly_Traffic_Est - a.Monthly_Traffic_Est);
-  let top10 = data.slice(0, 10);
+  let copiedData = [...data];
+  copiedData.sort((a, b) => b.Monthly_Traffic_Est - a.Monthly_Traffic_Est);
+  let top10 = copiedData.slice(0, 10);
   let Tool_Name = [];
   let Monthly_Traffic_Est = [];
   let Active_Users_Est = [];
@@ -163,160 +215,46 @@ function sortTable(colName) {
     currentData.sort((a, b) => a[colName] - b[colName]);
     sortOrder[colName] = true;
   }
+  sort = colName;
 
   render_table(currentData);
 
 }
 
+async function render_pie_chart(data) {
+  let counts = {};
+
+  for (let i = 0; i < data.length; i++) {
+    let model = data[i].Pricing_Model;
+
+    if (counts[model]) {
+      counts[model]++;
+    }
+    else {
+      counts[model] = 1;
+    }
+  }
+
+  const pieChartDisplay = document.getElementById('price_pie_chart');
+  let label = Object.keys(counts);
+  let value = Object.values(counts);
+
+  if (pricePieChart !== null) {
+    pricePieChart.destroy()
+  }
+
+  pricePieChart = new Chart(pieChartDisplay, {
+    type: 'pie',
+    data: {
+      labels: label,
+      datasets: [
+        {
+          data: value
+        }
+      ]
+    }
+  });
 
 
-// async function pricingSelect() {
 
-//   const res = price.value;
-//   const price_url = `http://127.0.0.1:8000/pricing/${res}`;
-//   console.log(price_url);
-
-//   try {
-//     const response = await fetch(price_url);
-//     const data = await response.json();
-//     console.log("data received:", data);
-//     const dataDisplay = document.getElementById("data_table_container");
-//     let dataHtml = `
-//     <table border ="1">
-//     <thead>
-// <tr>
-// <th>No.</th>
-// <th>Tool Name</th>
-// <th>Pricing Model</th>
-// <th>Starting Price (CAD)</th>
-// <th>Monthly Estimated Traffic</th>
-// <th>User Rating</th>
-// </tr>
-//     </thead>
-//     <tbody>
-//     `;
-//     for (let i = 0; i < data.length; i++) {
-//       dataHtml += `<tr>
-//       <td>${i + 1}</td>
-//       <td>${data[i].Tool_Name}</td>
-//       <td>${data[i].Pricing_Model}</td>
-//       <td>${data[i].Starting_Price_CAD}</td>
-//       <td>${data[i].Monthly_Traffic_Est}</td>
-//       <td>${data[i].User_Rating}</td>
-//       </tr>
-//       `;
-//     }
-
-//     dataHtml += `
-// </tbody>
-// </table>
-// `
-//     dataDisplay.innerHTML = dataHtml;
-//   } catch (error) {
-//     console.error("Fetch failed: ", error);
-//   }
-// }
-
-// search.addEventListener("click", pricingSelect);
-
-// async function draw_pie_chart() {
-//   const pie_char_url = `http://127.0.0.1:8000/pricing_distribution`;
-//   try {
-//     const response = await fetch(pie_char_url);
-//     const data = await response.json();
-//     console.log("data received:", data);
-//     const pieCharDisplay = document.getElementById("price_pie_chart");
-
-//     const label = [];
-//     const value = [];
-
-//     for (let i = 0; i < data.length; i++) {
-//       label.push(data[i].Pricing_Model);
-//       value.push(data[i].Count);
-//     }
-
-//     new Chart(pieCharDisplay, {
-//       type: 'pie',
-//       data: {
-//         labels: label,
-//         datasets: [
-//           {
-//             data: value
-//           }
-//         ]
-//       }
-//     });
-
-//   } catch (error) {
-//     console.error("Fetch failed: ", error);
-//   }
-// }
-// draw_pie_chart();
-
-// async function draw_bar_chart() {
-//   const traffic_url = `http://127.0.0.1:8000/top-traffic`;
-//   try {
-//     const response = await fetch(traffic_url);
-//     const data = await response.json();
-//     console.log("data received", data);
-//     const barChartDisplay = document.getElementById('top10_bar_chart');
-//     let label = [];
-//     let value = [];
-
-//     for (let i = 0; i < data.length; i++) {
-//       label.push(data[i].Tool_Name);
-//       value.push(data[i].Monthly_Traffic_Est);
-//     }
-
-//     new Chart(barChartDisplay, {
-//       type: 'bar',
-//       data: {
-//         labels: label,
-//         datasets: [
-//           {
-//             label: 'Top 10 Traffic',
-//             data: value
-//           }
-//         ]
-//       }
-//     });
-
-//   } catch (error) {
-//     console.error("Fetch failed: ", error);
-//   }
-
-
-// }
-
-// draw_bar_chart();
-
-// async function display_kpi() {
-//   const kpi_url = `http://127.0.0.1:8000/kpi-stats`;
-//   try {
-//     const response = await fetch(kpi_url);
-//     const data = await response.json();
-//     console.log("data received", data);
-//     const tool_Display = document.getElementById('total_tools');
-//     const max_traffic = document.getElementById('max_traffic');
-//     const avg_rating = document.getElementById('avg_rating');
-
-//     tool_Display.innerHTML = `
-//     <h3>Total Tools</h3>
-//     <p>${data.Total_Tools}</p>`
-
-//     max_traffic.innerHTML = `
-//     <h3>The Tool with the Max traffic</h3>
-//     <p>${data.Highest_Traffic}</p>`
-
-//     avg_rating.innerHTML = `
-//     <h3>Average User Rating</h3>
-//     <p>${data.Average_Rating.toFixed(2)}</p>`
-
-
-//   }
-//   catch (error) {
-//     console.error("Fetch failed: ", error);
-//   }
-// }
-
-// display_kpi();
+}
